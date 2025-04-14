cb474be7b8b9b1a6fbad1c6293c8a9347ccdf05f
Tahsin411934 <tahsin.mail.0503@gmail.com>
2025-04-14 22:31:41 +0600

    first commit

---
+++ resources/views/frontend/subnavbar.blade.php
@@ -0,0 +1,38 @@
+<div class="bg-[#F37021] h-10">
+<div class=" w-[90%] mx-auto">
+
+
+
+<div class=" flex justify-between items-center">
+    <div class=" text-white flex items-center gap-5">
+        <div class="pt-2">
+        <i class="fa-solid fa-phone"></i> <span class="text-[18px]  ">+8801828-7878</span> 
+        </div>
+        <div class="pt-2">
+        <i class="fa-solid fa-envelope"></i> <span class="text-[18px]  ">youthskills@yahoo.com</span> 
+        </div>
+        <div class="pt-2">
+        <i class="fa-solid fa-sun"></i> <span class="text-[18px]  ">Check Certificate Authenticity</span> 
+        </div>
+    
+    </div>
+    <div class=" text-white flex items-center gap-5">
+        <div class="pt-2">
+        <i class="fa-brands fa-facebook hover:bg-white p-1 hover:rounded-full hover:text-black"></i>
+        </div>
+        <div class="pt-2">
+        <i class="fa-solid fa-envelope hover:bg-white p-1 hover:rounded-full hover:text-black"></i>  
+        </div>
+        <div class="pt-2">
+        <i class="fa-brands fa-linkedin hover:bg-white p-1 hover:rounded-full hover:text-black"></i>
+        </div>
+        <div class="pt-2">
+        <i class="fa-brands fa-tiktok hover:bg-white p-1 hover:rounded-full hover:text-black"></i>
+        </div>
+        <div class="pt-2  hover:text-black">
+        <i class="fa-brands fa-instagram hover:bg-white p-1 hover:rounded-full"></i>
+        </div>
+    
+    </div>
+</div>
+</div></div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+<x-guest-layout>
+    <div class="mt-64 flex justify-center items-center ">
+        <span class="shadow-lg">
+        This will be the main website Home page
+        </span>
+            
+    </div>
+</x-guest-layout>
